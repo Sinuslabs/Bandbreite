6cfbc86d70ec23c68c251a148840fe0f47bc470f
oskar <daeh@tuta.io>
2024-05-25 22:38:07 +0700

    header lottie animation with clicks

--- Scripts/Header.js
+++ Scripts/Header.js
@@ -8,24 +8,35 @@ namespace Header {
 	Textures_btn.setControlCallback(onTextures);
 	Theme_btn.setControlCallback(onTheme);
 	
-	
+	Product_Logo_pnlbtn.setPaintRoutine(function(g){
+		var data = this.getValue();
+		var a = this.getLocalBounds(1);
+		Console.print(trace(data));
+		if (data.hover) {
+			g.setColour(Colours.withAlpha(Colours.black, 0.2));
+			g.fillRoundedRectangle(a, Primitives.BorderRadius.sm);
+		}
+	});
 	Product_Logo_pnlbtn.setValue({value: false});
 	Product_Logo_pnlbtn.setMouseCallback(function(event) {
+		
 		var data = this.getValue();
-		if (event.mouseUp) {
-			var props = {
-				value: !data.value
-			};
+		var props = {
+			value: data.value,
+			hover: event.hover
+		};
+		
+		if (event.clicked && !event.mouseUp) {
+			props.value = !data.value;
 			if (props.value) {
-				LogoAnimation.init();			
 				Router.goTo('About');
 				Textures_btn.setValue(0);
 			} else {
 				Router.goTo('Tape');
 			}
-			this.setValue(props);
 		}
-
+		this.setValue(props);
+		this.repaint();
 	});
 	
 	inline function onTextures(component, value) {
